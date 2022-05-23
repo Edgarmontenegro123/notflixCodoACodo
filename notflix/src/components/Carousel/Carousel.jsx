44b26cb619939cadef0647ca7a6f1f2f8416170f
Edgar Montenegro <edgarmontenegro321@gmail.com>
2022-05-23 11:47:34 -0300

    ✨ Add Carousel component

---
+++ notflix/src/components/Carousel/Carousel.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Slider from 'react-slick';
+import Card from '../Card/Card';
+
+const Carousel = () => {
+  return (
+    <>
+      <Slider>
+        <Card />
+      </Slider>
+    </>
+  )
+}
+
+export default Carousel;
