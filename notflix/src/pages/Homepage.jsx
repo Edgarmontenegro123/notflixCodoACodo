45692820d2337c2f0c90569dfe82f18c9ddf21d6
Edgar Montenegro <edgarmontenegro321@gmail.com>
2022-05-25 04:13:08 -0300

    ✨ Add Carousels to Homepage view

--- notflix/src/pages/Homepage.jsx
+++ notflix/src/pages/Homepage.jsx
@@ -3,12 +3,15 @@ import Banner from '../components/Banner/Banner';
 import Carousel from '../components/Carousel/Carousel';
 import 'slick-carousel/slick/slick.css'; 
 import 'slick-carousel/slick/slick-theme.css';
+import { apiEntity } from '../apiConfig';
 
 const Homepage = () => {
   return (
     <>
       <Banner />
-      <Carousel />
+      <Carousel entity = {apiEntity.popularMovies} title = {'Películas Populares'}/>
+      <Carousel entity = {apiEntity.popularTv} title = {'Series Populares'}/>
+      <Carousel entity = {apiEntity.topRatedMovies} title = {'Top Películas'}/>
     </>
   )
 }
