e86bcd7d16c3550d7e2a4de9c2efb813b60b234b
Edgar Montenegro <edgarmontenegro321@gmail.com>
2022-05-25 04:12:00 -0300

    ✨ Add Carousel component

--- notflix/src/components/Carousel/Carousel.jsx
+++ notflix/src/components/Carousel/Carousel.jsx
@@ -1,13 +1,32 @@
 import React from 'react';
 import Slider from 'react-slick';
+import { apiEntity } from '../../apiConfig';
+import useApi from '../../hooks/useApi';
 import Card from '../Card/Card';
+import Separator from '../Separator/Separator';
+import { settingSliders } from './settingsSlider';
 
-const Carousel = () => {
+const Carousel = ({entity, title}) => {
+
+  const [movies, loading, error] = useApi(entity);
+
   return (
     <>
-      <Slider>
-        <Card />
+    <h2 style = {{color: 'whitesmoke'}}>{title}</h2>
+      <Slider {...settingSliders} style={{backgroundColor: 'blue'}}>
+        {
+          loading? (
+            <h1>Loading...</h1>
+          ) : (
+            movies.map((movie) => (
+              <Card key = {movie.id} 
+                    title = {movie.title || movie.name}
+                    imgPath = {movie.backdrop_path} />
+            ))
+          )
+        }
       </Slider>
+      <Separator height={'20px'}/>
     </>
   )
 }
